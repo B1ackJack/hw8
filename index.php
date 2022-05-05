07d36654573d6f37fd5b5e1cb22debb49f694a13
b1ackjack <b1ackjack@protonmail.ch>
2022-05-05 19:54:05 -0400

    initial login form

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
+<?php
 /*
  * This is the initial login page landing
  *
  */
+?>
+<html>
+    <head>
+        <title>WAPT HW 8 - Simple Login</title>
+    </head>
+    <body>
+        <h2>Login</h2>
+        <form name="login-form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <label>Username</label>
+            <input type="text" name="username">
+            <label>Password</label>
+            <input type="password" name="password">
+            <input type="submit" value="Login">
+        </form>
+    </body>
+</html>
