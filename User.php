b8e6d7ccc9e7404ce41295a3da63647452f7e9e4
b1ackjack <b1ackjack@protonmail.ch>
2022-05-05 12:24:07 -0400

    base user class

---
+++ User.php
@@ -0,0 +1,25 @@
+<?php
+
+class User
+{
+    public function get(){
+
+    }
+
+    public function register(){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function delete(){
+
+    }
+
+}
