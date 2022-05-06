aef5313798ffa8b4904e00d8e108c0f0d98ecd3a
b1ackjack <b1ackjack@protonmail.ch>
2022-05-05 20:59:35 -0400

    restructure directory for classes + added user table

--- Classes/DbConnection.php
+++ Classes/DbConnection.php
@@ -2,6 +2,31 @@
  /*
   * DbConnetion Class
   */
- class DbConnection {
+ class DbConnections {
+     var $host   = ""; //database server
+     var $user     = ""; //database login name
+     var $pass     = ""; //database login password
+     var $database = ""; //database name
 
+     public $link;
+
+     public function Database($host, $user, $pass, $database)
+     {
+         $this->host=$host;
+         $this->user=$user;
+         $this->pass=$pass;
+         $this->database=$database;
+     }
+
+     public function connect()
+     {
+         $this->link = new mysqli($this->host,$this->user,$this->pass,$this->database);
+         if (mysqli_connect_error())
+         {
+             echo mysqli_connect_error();
+             exit();
+         } else {
+             return $this->link;
+         }
+     }
  }
